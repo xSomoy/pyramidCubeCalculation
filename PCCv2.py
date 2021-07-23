75baab87807abf1ae08113fefce73d762281b704
John Deadman <xsomoy@tutanota.com>
2021-07-23 19:41:31 +0600

    pyraaaaa

---
+++ PCCv2.py
@@ -0,0 +1,10 @@
+# Pyramid Cube Calculation Based On The Number of Cube in a Base Layer
+# By John Deadman 14:40 23/07/2021 +0600 GMT
+
+base = int(input("How many cubes in base?\n"))
+total = 0
+layer = 0
+while layer != base:
+    total  = total + (base-layer)
+    layer=layer+1
+print("There are total " + str(total) + " cubes") 
