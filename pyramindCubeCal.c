d4b5fe2c1ed98f6f71b32d867dbd0bf6f158ae2a
John Deadman <xsomoy@tutanota.com>
2021-07-23 13:50:41 +0600

    pyraaaaa

--- pyramindCubeCal.c
+++ pyramindCubeCal.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
-main(){
-    int base,layer=1,total=0;
+main()
+{
+    int base, layer = 1, total = 0;
     scanf("%d", &base);
-    while(layer != base)
+    while (layer != base)
     {
-        total=base + (base - layer);
-        layer=layer-1;
+        total = base + (base - layer);
+        layer = layer - 1;
     }
-    printf("%d",total);
+    printf("%d", total);
     getch();
 }
