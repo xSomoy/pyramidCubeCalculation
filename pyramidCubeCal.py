c5ff7da9b77bdb5215189d337d7784e5335b92fe
John Deadman <xsomoy@tutanota.com>
2021-07-23 14:20:45 +0600

    pyraaaaa

--- pyramidCubeCal.py
+++ pyramidCubeCal.py
@@ -2,11 +2,11 @@ base = input()
 base = int(base)
 total = 0
 layer=1
-# while layer <= base:
-#     total = base + (base-layer)
-#     layer=layer+1
-
-for layer < base:
+while layer <= base:
     total = base + (base-layer)
     layer=layer+1
+
+# for layer is < base:
+#     total = base + (base-layer)
+#     layer=layer+1
 print(total) 
